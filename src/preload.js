f65ed03ee01348f2ad2d871f2cfdf627e0fab2bd
VadimNastoyashchy <Vadim.89@ukr.net>
2024-07-10 12:38:25 +0300

    Ref logic

--- src/preload.js
+++ src/preload.js
@@ -2,7 +2,19 @@
 const fs = require('fs')
 const path = require('node:path')
 
+let filesToProcess = null
+
 window.addEventListener('DOMContentLoaded', () => {
+  const splitBtn = document.getElementById('splitBtn')
+  const progressText = document.getElementById('progressBar')
+
+  splitBtn.addEventListener('click', async () => {
+    splitBtn.disabled = true
+    progressText.innerText = 'Splitting in progress...'
+    await splitJsonArray(filesToProcess) // Run the split operation
+    progressText.innerText = 'Splitting complete!'
+    splitBtn.disabled = false
+  })
   const dropArea = document.getElementById('drop-area')
 
   // Prevent default drag behaviors
@@ -25,7 +37,18 @@ window.addEventListener('DOMContentLoaded', () => {
     const dt = e.dataTransfer
     const files = dt.files
 
-    splitJsonArray(files)
+    // Add opened file names to list
+    const fileNamesDiv = document.getElementById('fileNames')
+    fileNamesDiv.innerHTML = ''
+    Array.from(files).forEach((file) => {
+      let p = document.createElement('p')
+      p.textContent = file.name
+      fileNamesDiv.append(p)
+    })
+
+    filesToProcess = files
+    splitBtn.disabled = false
+    progressText.innerText = ''
   }
 
   function preventDefaults(e) {
