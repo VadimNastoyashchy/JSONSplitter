658dcd9a8bd60157d92c70217d88b24c0eac7948
VadimNastoyashchy <Vadim.89@ukr.net>
2024-07-10 11:24:15 +0300

    Add core logic

--- src/main.js
+++ src/main.js
@@ -8,6 +8,8 @@ const createWindow = () => {
     height: 600,
     webPreferences: {
       preload: path.join(__dirname, '../src/preload.js'),
+      nodeIntegration: true,
+      contextIsolation: false,
     },
   })
 
--- src/preload.js
+++ src/preload.js
@@ -1,4 +1,7 @@
 /* eslint-disable space-before-function-paren */
+const fs = require('fs')
+const path = require('node:path')
+
 window.addEventListener('DOMContentLoaded', () => {
   const dropArea = document.getElementById('drop-area')
 
@@ -18,29 +21,58 @@ window.addEventListener('DOMContentLoaded', () => {
   // Handle dropped files
   dropArea.addEventListener('drop', handleDrop, false)
 
+  async function handleDrop(e) {
+    const dt = e.dataTransfer
+    const files = dt.files
+
+    splitJsonArray(files)
+  }
+
   function preventDefaults(e) {
     e.preventDefault()
     e.stopPropagation()
   }
 
-  function handleDrop(e) {
-    const dt = e.dataTransfer
-    const files = dt.files
-
-    console.log(files)
-
-    Array.from(files).forEach((file) => {
-      console.log('File Path:', file.path)
-    })
-  }
-
   function highlight(e) {
-    // Add some style to highlight
     dropArea.style.background = '#ddd'
   }
 
   function unhighlight(e) {
-    // Remove highlighted style
     dropArea.style.background = 'none'
   }
 })
+
+const parseMultipleJson = (files) => {
+  const multipleJson = []
+  for (let index = 0; index < files.length; index++) {
+    const bufferObj = fs.readFileSync(files[index].path)
+    multipleJson.push(JSON.parse(bufferObj.toString()))
+  }
+  return multipleJson
+}
+
+const splitJsonArray = (files, limit = 5000) => {
+  try {
+    const source = parseMultipleJson(files)
+    const flattedSource = source?.flat()
+
+    const totalParts = Math.ceil(flattedSource.length / limit)
+    for (let i = 0; i < totalParts; i++) {
+      const partArray = flattedSource.slice(i * limit, (i + 1) * limit)?.flat()
+      const partFileName = path.join(__dirname, `../output/part${i + 1}.json`)
+      fs.writeFileSync(
+        partFileName,
+        JSON.stringify(partArray, null, 2),
+        (err) => {
+          if (err) {
+            console.error(`Error writing file ${partFileName}:`, err)
+          } else {
+            console.log(`Successfully wrote ${partFileName}`)
+          }
+        }
+      )
+    }
+  } catch (e) {
+    console.error('Error parsing JSON:', e)
+  }
+}
